573029f4932bcd914ebce1a12bcfebc45de1a241
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2021-02-19 10:11:07 +0100

    added api.js

---
+++ scripts/api.js
@@ -0,0 +1,14 @@
+export function getData(url) {
+    return fetch(url)
+}
+
+// Code adapted from Roeland
+export function getDetailData(array) {
+    const swDetailData = array.map(singleEndpoint => {
+        singleEndpoint.forEach(endpoint => {
+            return fetch(endpoint)
+        });
+    });
+    return Promise.all(swDetailData);
+}
+
